e92b445c75a0599980cba3ef0db34074d21e4d23
Whitedoggy <nubuli19@gmail.com>
2025-07-14 16:08:47 +0900

    close #19

--- src/main/java/com/ll/domain/member/entity/Member.java
+++ src/main/java/com/ll/domain/member/entity/Member.java
@@ -2,8 +2,10 @@ package com.ll.domain.member.entity;
 
 import com.ll.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Entity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Getter
@@ -14,7 +16,19 @@ public class Member extends BaseEntity {
     private String password;
     private String name;
     private String address;
-    private enum Role {
+    private Role role;
+
+    public enum Role {
         ADMIN, USER
+    }
+
+    @Builder
+    public Member(String email, String user_id, String password, String name, String address, Role role) {
+        this.email = email;
+        this.user_id = user_id;
+        this.password = password;
+        this.name = name;
+        this.address = address;
+        this.role = role;
     }
 }
--- src/main/java/com/ll/domain/member/repository/MemberRepository.java
+++ src/main/java/com/ll/domain/member/repository/MemberRepository.java
@@ -1,4 +1,12 @@
 package com.ll.domain.member.repository;
 
-public interface MemberRepository {
+import com.ll.domain.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Integer> {
+    Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByName(String name);
 }
