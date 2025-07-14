8317a408842da5baaefe181df513526333ac7627
정동하 <jeongdongha@jeongdonghaui-MacBookAir.local>
2025-07-14 14:30:18 +0900

    Menu get

--- src/main/java/com/ll/domain/menu/controller/ApiV1MenuReadController.java
+++ src/main/java/com/ll/domain/menu/controller/ApiV1MenuReadController.java
@@ -1,4 +1,40 @@
 package com.ll.domain.menu.controller;
 
+
+import com.ll.domain.menu.dto.MenuDto;
+import com.ll.domain.menu.entity.Menu;
+import com.ll.domain.menu.service.MenuService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/menus")
+@RequiredArgsConstructor
 public class ApiV1MenuReadController {
+    private final MenuService menuService;
+
+    @GetMapping
+    @Transactional(readOnly = true)
+    public List<MenuDto> getItems(){
+        List<Menu> items = menuService.findAll();
+
+        return items
+                .stream()
+                .map(MenuDto::new)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    @Transactional(readOnly = true)
+    public MenuDto getItem(@PathVariable Long id) {
+        Menu menu = menuService.findById(id).get();
+
+        return new MenuDto(menu);
+    }
 }
--- src/main/java/com/ll/domain/menu/dto/MenuDto.java
+++ src/main/java/com/ll/domain/menu/dto/MenuDto.java
@@ -1,4 +1,17 @@
 package com.ll.domain.menu.dto;
 
-public class MenuDto {
+import com.ll.domain.menu.entity.Menu;
+
+public record MenuDto (
+        Long id,
+        String name,
+        String image,
+        int price
+
+){
+    public MenuDto (Menu menu){
+        this(
+                menu.getId(), menu.getName(), menu.getImage(), menu.getPrice()
+        );
+    }
 }
