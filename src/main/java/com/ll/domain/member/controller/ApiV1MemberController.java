35888fb003efafa73b8dd7cfbb0553480ccaf825
Whitedoggy <nubuli19@gmail.com>
2025-07-15 10:16:57 +0900

    member CRUD 및 로그인 기능 일부 구현, close #33

--- src/main/java/com/ll/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/ll/domain/member/controller/ApiV1MemberController.java
@@ -3,11 +3,17 @@ package com.ll.domain.member.controller;
 import com.ll.domain.member.dto.MemberDto;
 import com.ll.domain.member.entity.Member;
 import com.ll.domain.member.service.MemberService;
+import com.ll.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import com.ll.global.exception.ServiceException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -16,35 +22,114 @@ import java.util.Optional;
 public class ApiV1MemberController {
     private final MemberService memberService;
 
-    @PostMapping
-    public ResponseEntity<MemberDto.Response> join(@RequestBody MemberDto.JoinRequest dto){ // 회원 가입
+    record SignUpRequest(
+            @Email
+            @NotBlank
+            String email,
+
+            @NotBlank
+            String password,
+
+            @NotBlank
+            String name,
+
+            @NotBlank
+            String address
+    ) {
+    }
+
+    @PostMapping("/signup/user") // 사용자 회원가입
+    public RsData<MemberDto> signUpUser(@Valid @RequestBody SignUpRequest signUpRequest){
         Member member = memberService.addUserMember(
-                dto.getEmail(),
-                dto.getUser_id(),
-                dto.getPassword(),
-                dto.getName(),
-                dto.getAddress()
+                signUpRequest.email(),
+                signUpRequest.password(),
+                signUpRequest.name(),
+                signUpRequest.address()
         );
 
-        MemberDto.Response response = new MemberDto.Response(member);
-        return ResponseEntity.created(URI.create("/api/v1/members/" + member.getId())).body(response);
+        return new RsData<>(
+                "201-1",
+                "%s 사용자님 환영합니다. 회원가입이 완료되었습니다.".formatted(member.getName()),
+                new MemberDto(member)
+        );
+    }
+
+    @PostMapping("/signup/admin") // 관리자 회원 가입
+    public RsData<MemberDto> signUpAdmin(@Valid @RequestBody SignUpRequest signUpRequest){
+        Member member = memberService.addAdminMember(
+                signUpRequest.email(),
+                signUpRequest.password(),
+                signUpRequest.name(),
+                signUpRequest.address()
+        );
+
+        return new RsData<>(
+                "201-2",
+                "%s 관리자님 환영합니다. 회원가입이 완료되었습니다.".formatted(member.getName()),
+                new MemberDto(member)
+        );
+    }
+
+    record LoginRequest (
+            @Email
+            @NotBlank
+            String email,
+
+            @NotBlank
+            String password
+    ) {
+    }
+
+    record LoginResponse (
+            MemberDto item,
+            String apiKey,
+            String token
+    ) {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody MemberDto.LoginRequest dto){ // 로그인
-        Optional<Member> memberOptional = memberService.login(dto.getEmail(), dto.getPassword());
+    public RsData<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest){ // 로그인
+        Member member = memberService.findByEmail(loginRequest.email())
+                .orElseThrow(() -> new ServiceException("401-1", "가입되지 않은 이메일입니다."));
 
-        if(memberOptional.isEmpty()){
-            return ResponseEntity.badRequest().body("이메일 또는 비밀번호가 올바르지 않습니다.");
-        }
+        memberService.checkPassword(
+                member,
+                loginRequest.password()
+        );
 
-        return ResponseEntity.ok(new MemberDto.Response(memberOptional.get()));
+        return new RsData<>(
+                "200-1",
+                "%s님 환영합니다.".formatted(member.getName()),
+                new LoginResponse(
+                        new MemberDto(member),
+                        "api-key",
+                        "token"
+                )
+        );
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<MemberDto.Response> findById(@PathVariable Integer id){ // id로 멤버 찾기
-        return memberService.findById(id)
-                .map(member -> ResponseEntity.ok(new MemberDto.Response(member)))
-                .orElse(ResponseEntity.notFound().build());
+    @PostMapping("/logout")
+    public RsData<Void> logout(){
+        return new RsData<>(
+                "200-1",
+                "로그아웃 되었습니다."
+        );
+    }
+
+    @GetMapping("/admin") // 다건 조회
+    public List<MemberDto> getMembers(){ // id로 멤버 찾기
+        List<Member> members = memberService.findAll();
+
+        return members
+                .stream()
+                .map(MemberDto::new)
+                .toList();
+    }
+
+    @GetMapping("/admin/{id}") // 단건 조회 ( id 기반 )
+    public MemberDto getMember(@PathVariable Integer id){ // id로 멤버 찾기
+        Member member = memberService.findById(id).get();
+
+        return new MemberDto(member);
     }
 }
--- src/main/java/com/ll/domain/member/entity/Member.java
+++ src/main/java/com/ll/domain/member/entity/Member.java
@@ -1,18 +1,17 @@
 package com.ll.domain.member.entity;
 
 import com.ll.global.jpa.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
 @NoArgsConstructor
 public class Member extends BaseEntity {
+    @Column(unique = true)
     private String email;
-    private String user_id;
+    // String user_id;
     private String password;
     private String name;
     private String address;
@@ -23,9 +22,9 @@ public class Member extends BaseEntity {
     }
 
     @Builder
-    public Member(String email, String user_id, String password, String name, String address, Role role) {
-        this.email = email;
-        this.user_id = user_id;
+    public Member(String email, String password, String name, String address, Role role) {
+        this.email = email; // id로 사용 예정
+        // this.user_id = user_id; // 사용처 없음. identity 키의 경우 BaseEntity에서 이미 선언됨.
         this.password = password;
         this.name = name;
         this.address = address;
