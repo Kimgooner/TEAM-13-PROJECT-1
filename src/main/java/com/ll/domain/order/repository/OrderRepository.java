2ff84443c0d523ab8edcc03d144b688283f15d17
정동하 <jeongdongha@jeongdonghaui-MacBookAir.local>
2025-07-17 14:25:35 +0900

    order 리팩토링

--- src/main/java/com/ll/domain/order/repository/OrderRepository.java
+++ src/main/java/com/ll/domain/order/repository/OrderRepository.java
@@ -2,6 +2,18 @@ package com.ll.domain.order.repository;
 
 import com.ll.domain.order.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    //N+1문제 해결위한 fetch join
+    @Query("SELECT o FROM Order o JOIN FETCH o.orderItems oi JOIN FETCH oi.product WHERE o.id = :id")
+    Optional<Order> findByIdWithItems(@Param("id") int id);
+
+    @Query("SELECT o FROM Order o")
+    List<Order> findAllWithItems();
 }
